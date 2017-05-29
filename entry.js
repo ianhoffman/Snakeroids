9ad8efe517c59df0d625d8dee206499656382446
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-28 18:27:13 -0700

    sprite asteroids spin!

--- entry.js
+++ entry.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.style.width = "800px";
     canvas.style.height = "800px";
     const ctx = canvas.getContext('2d');
+
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    
     const game = new GameView(ctx);
     game.start();
 });

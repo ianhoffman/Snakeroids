0def4c81c1ee8bf1fcc4f501f2c752a89ceed7f2
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-28 17:59:02 -0700

    render basic sprite image, no animation yet

--- entry.js
+++ entry.js
@@ -2,6 +2,8 @@ import GameView from './lib/game_view.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas');
+    canvas.style.width = "800px";
+    canvas.style.height = "800px";
     const ctx = canvas.getContext('2d');
     const game = new GameView(ctx);
     game.start();

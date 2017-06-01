a8fb317cb8dc3f9b5499763df12a6cd807e02256
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-31 19:51:15 -0700

    play again works

--- entry.js
+++ entry.js
@@ -52,11 +52,6 @@ import GameView from './lib/game_view.js';
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas');
 
-    const ctx = canvas.getContext('2d');
-    canvas.webkitImageSmoothingEnabled = false;
-    canvas.mozImageSmoothingEnabled = false;
-    canvas.imageSmoothingEnabled = false;
-
     document.getElementsByClassName('title-container')[0].addEventListener('click', e => {
             e.preventDefault();
             if(e.target.id==='start-button') {
@@ -66,8 +61,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('instructions-container').style.display = 'flex';
                 document.getElementById('start-button').addEventListener('click', e2 => {
                     e2.preventDefault();
-                    const game = new GameView(ctx);
+                    const game = new GameView(canvas);
                     document.getElementById('instructions-container').style.display = 'none';
+                    e2.target.id = '';
                     game.start();
                 });
             }
